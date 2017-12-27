09bf033c91f4d1ac7cb83cd689f728525af6c68b
Franck Lebeau <fl.franck.lebeau+github@gmail.com>
2017-12-27 17:31:07 +0100

    add test for initial placement of sites

--- test/d3-voronoi-map-test.js
+++ test/d3-voronoi-map-test.js
@@ -55,14 +55,41 @@ tape("voronoiMap.minWeightRatio(...) should set the specified ratio", function(t
   test.end();
 });
 
+tape("voronoiMap.initPlacement(...)", function(test) {
+  test.test("voronoiMap.initPlacement(...) should set the specified initial placement strategy", function(test) {
+    var voronoiMap = d3VoronoiMap.voronoiMap();
+    var datum = {weight: 1, xPos: 0.5, yPos: 0.5};
+    var newStrategy = function(d,i,clippingPolygon){ return [d.xPos, d.yPos]; };
+
+    test.equal(voronoiMap.initPlacement(newStrategy), voronoiMap);
+    test.deepEqual(voronoiMap.initPlacement()(datum), [0.5, 0.5]);
+    test.end();
+  });
+
+  test.test("voronoiMap.initPlacement(...) should fallback to a random placement if specified strategy places sites outside the clipping polygon", function(test) {
+    var voronoiMap = d3VoronoiMap.voronoiMap(),
+        data = [{weight: 1, xPos: 2, yPos: 2}],
+        newStrategy = function(d,i,clippingPolygon){ return [d.xPos, d.yPos]; };
+        res = voronoiMap.maxIterationCount(0).initPlacement(newStrategy)(data),
+        initX = res.polygons[0].site.originalObject.x,
+        initY = res.polygons[0].site.originalObject.y;
+
+    test.notEqual(initX, 2);
+    test.ok(initX>0 && initX<1);
+    test.notEqual(initY, 2);
+    test.ok(initY>0 && initY<1);
+    test.end();
+  });
+});
+
 tape("voronoiMap.(...) should compute Voronoï map", function(test) {
   test.test("basic use case", function(test) {
-    var voronoiMap = d3VoronoiMap.voronoiMap().maxIterationCount(1),
+    var voronoiMap = d3VoronoiMap.voronoiMap().maxIterationCount(2),
         data = [{weight: 1}, {weight: 1}],
         res = voronoiMap(data);
 
     test.equal(res.polygons.length, 2);
-    test.equal(res.iterationCount, 1);
+    test.equal(res.iterationCount, 2);
     test.ok(res.convergenceRatio);
     test.end();
   });
