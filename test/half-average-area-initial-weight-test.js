e6fab2a458e00432f7d3ff9cf350255d58250e0e
Kcnarf <fl.franck.lebeau@gmail.com>
2019-07-26 12:10:28 +0200

    [spec] stop() any launched simluation, and use 'const' and 'let' (instead of 'var')

--- test/half-average-area-initial-weight-test.js
+++ test/half-average-area-initial-weight-test.js
@@ -1,30 +1,40 @@
-var tape = require("tape"),
-  d3VoronoiMapInitialWeightHalfAverageArea = require("../build/initial-weight-policies/half-average-area"),
-  d3VoronoiMap = require("../build/d3-voronoi-map");
+const tape = require('tape'),
+  d3VoronoiMapInitialWeightHalfAverageArea = require('../build/initial-weight-policies/half-average-area'),
+  d3VoronoiMap = require('../build/d3-voronoi-map');
 
-tape("initial-weight-policies/half-average-area(...)", function (test) {
-  test.test("initial-weight-policies/half-average-area(...) should depends on clipping polygon", function (test) {
-    var initialWeightHalfAverageArea = d3VoronoiMapInitialWeightHalfAverageArea(),
-      data = [{
-        weight: 1
-      }],
-      voronoiMapSimulation = d3VoronoiMap.voronoiMapSimulation(data).size([2, 3]);
+tape('initial-weight-policies/half-average-area(...)', function(test) {
+  test.test('initial-weight-policies/half-average-area(...) should depends on clipping polygon', function(test) {
+    const initialWeightHalfAverageArea = d3VoronoiMapInitialWeightHalfAverageArea(),
+      data = [
+        {
+          weight: 1
+        }
+      ],
+      voronoiMapSimulation = d3VoronoiMap
+        .voronoiMapSimulation(data)
+        .size([2, 3])
+        .stop();
 
     test.equal(initialWeightHalfAverageArea(data[0], 0, data, voronoiMapSimulation), 3);
     test.end();
   });
 
-  test.test("initial-weight-policies/half-average-area(...) should depends on number of data", function (test) {
-    var initialWeightHalfAverageArea = d3VoronoiMapInitialWeightHalfAverageArea(),
-      data = [{
-        weight: 1
-      }, {
-        weight: 1
-      }],
-      voronoiMapSimulation = d3VoronoiMap.voronoiMapSimulation(data).size([2, 3]);
+  test.test('initial-weight-policies/half-average-area(...) should depends on number of data', function(test) {
+    const initialWeightHalfAverageArea = d3VoronoiMapInitialWeightHalfAverageArea(),
+      data = [
+        {
+          weight: 1
+        },
+        {
+          weight: 1
+        }
+      ],
+      voronoiMapSimulation = d3VoronoiMap
+        .voronoiMapSimulation(data)
+        .size([2, 3])
+        .stop();
 
     test.equal(initialWeightHalfAverageArea(data[0], 0, data, voronoiMapSimulation), 1.5);
     test.end();
   });
-
 });
--- test/random-initial-position-test.js
+++ test/random-initial-position-test.js
@@ -1,13 +1,15 @@
-var tape = require("tape"),
-  d3VoronoiMapInitialPositionRandom = require("../build/initial-position-policies/random"),
-  d3VoronoiMap = require("../build/d3-voronoi-map");
+var tape = require('tape'),
+  d3VoronoiMapInitialPositionRandom = require('../build/initial-position-policies/random'),
+  d3VoronoiMap = require('../build/d3-voronoi-map');
 
-tape("initial-position-policies/pie()(...) default test", function (test) {
-  var initialPositionRandom = d3VoronoiMapInitialPositionRandom(),
-    data = [{
-      weight: 1
-    }],
-    voronoiMapSimulation = d3VoronoiMap.voronoiMapSimulation(data),
+tape('initial-position-policies/pie()(...) default test', function(test) {
+  const initialPositionRandom = d3VoronoiMapInitialPositionRandom(),
+    data = [
+      {
+        weight: 1
+      }
+    ],
+    voronoiMapSimulation = d3VoronoiMap.voronoiMapSimulation(data).stop(),
     initCoords = initialPositionRandom(data[0], 0, data, voronoiMapSimulation);
 
   test.ok(initCoords[0] > 0);
@@ -18,16 +20,18 @@ tape("initial-position-policies/pie()(...) default test", function (test) {
   test.end();
 });
 
-tape("initial-position-policies/pie(...) and clipping polygon", function (test) {
-  test.test("initial-position-policies/pie(...) should depends on clipping polygon", function (test) {
-    var initialPositionRandom = d3VoronoiMapInitialPositionRandom(),
-      data = [{
-        weight: 1
-      }],
-      voronoiMapSimulation = d3VoronoiMap.voronoiMapSimulation(data).extent([
-        [1, 1],
-        [2, 2]
-      ]),
+tape('initial-position-policies/pie(...) and clipping polygon', function(test) {
+  test.test('initial-position-policies/pie(...) should depends on clipping polygon', function(test) {
+    const initialPositionRandom = d3VoronoiMapInitialPositionRandom(),
+      data = [
+        {
+          weight: 1
+        }
+      ],
+      voronoiMapSimulation = d3VoronoiMap
+        .voronoiMapSimulation(data)
+        .extent([[1, 1], [2, 2]])
+        .stop(),
       initCoords = initialPositionRandom(data[0], 0, data, voronoiMapSimulation);
 
     test.ok(initCoords[0] > 1);
@@ -38,26 +42,25 @@ tape("initial-position-policies/pie(...) and clipping polygon", function (test) 
     test.end();
   });
 
-  test.test("initial-position-policies/randomPolicy()(...) should handle clipping polygon updates", function (test) {
-    var initialPositionRandom = d3VoronoiMapInitialPositionRandom(),
-      data = [{
-        weight: 1
-      }],
-      voronoiMapSimulation = d3VoronoiMap.voronoiMapSimulation(data).extent([
-        [1, 1],
-        [2, 2]
-      ]),
-      initCoords = initialPositionRandom(data[0], 0, data, voronoiMapSimulation);
+  test.test('initial-position-policies/randomPolicy()(...) should handle clipping polygon updates', function(test) {
+    const initialPositionRandom = d3VoronoiMapInitialPositionRandom(),
+      data = [
+        {
+          weight: 1
+        }
+      ],
+      voronoiMapSimulation = d3VoronoiMap
+        .voronoiMapSimulation(data)
+        .extent([[1, 1], [2, 2]])
+        .stop();
+    let initCoords = initialPositionRandom(data[0], 0, data, voronoiMapSimulation);
 
     test.ok(initCoords[0] > 1);
     test.ok(initCoords[0] < 2);
     test.ok(initCoords[1] > 1);
     test.ok(initCoords[1] < 2);
 
-    voronoiMapSimulation.extent([
-      [2, 2],
-      [3, 3]
-    ])
+    voronoiMapSimulation.extent([[2, 2], [3, 3]]);
     initCoords = initialPositionRandom(data[0], 0, data, voronoiMapSimulation);
 
     test.ok(initCoords[0] > 2);
@@ -69,15 +72,21 @@ tape("initial-position-policies/pie(...) and clipping polygon", function (test) 
   });
 });
 
-tape("initial-position-policies/randomPolicy()(...) should use expected prng", function (test) {
-  var myprng = function () { // not a prng, but do the trick for the test!
-    return .5;
+tape('initial-position-policies/randomPolicy()(...) should use expected prng', function(test) {
+  const myprng = function() {
+    // not a prng, but do the trick for the test!
+    return 0.5;
   };
-  var initialPositionRandom = d3VoronoiMapInitialPositionRandom(),
-    data = [{
-      weight: 1
-    }],
-    voronoiMapSimulation = d3VoronoiMap.voronoiMapSimulation(data).prng(myprng),
+  const initialPositionRandom = d3VoronoiMapInitialPositionRandom(),
+    data = [
+      {
+        weight: 1
+      }
+    ],
+    voronoiMapSimulation = d3VoronoiMap
+      .voronoiMapSimulation(data)
+      .prng(myprng)
+      .stop(),
     initCoords = initialPositionRandom(data[0], 0, data, voronoiMapSimulation);
 
   test.equal(initCoords[0], 0.5);
